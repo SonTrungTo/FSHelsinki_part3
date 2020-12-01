bd76004336eb6d05c2ae9414cd801dc6f57c50c6
SonTrungTo <son.trung.to@gmail.com>
2020-12-01 15:55:26 +0200

    exercise 3.1 in progress...

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const persons = [
+    
+];
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running now on ${PORT}`);
+});
