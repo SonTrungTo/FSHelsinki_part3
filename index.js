a931f8daa1336dcc5c976169b1f24f5ce4452d8f
SonTrungTo <son.trung.to@gmail.com>
2020-12-03 15:41:13 +0200

    3.13-3.14 solved!

--- index.js
+++ index.js
@@ -2,29 +2,30 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+const Person = require('./models/person');
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
-];
+// let persons = [
+//     {
+//         id: 1,
+//         name: "Arto Hellas",
+//         number: "040-123456"
+//     },
+//     {
+//         id: 2,
+//         name: "Ada Lovelace",
+//         number: "39-44-5323523"
+//     },
+//     {
+//         id: 3,
+//         name: "Dan Abramov",
+//         number: "12-43-234345"
+//     },
+//     {
+//         id: 4,
+//         name: "Mary Poppendick",
+//         number: "39-23-6423122"
+//     }
+// ];
 
 const generateId = () => {
     return Math.round(Math.random() * new Date().valueOf());
@@ -39,7 +40,9 @@ morgan.token('body', (req, res) => {
 });
 app.use(morgan(':body'));
 app.get('/api/persons', (req, res) => {
-    return res.json(persons);
+    Person.find({}).then(persons => {
+        res.json(persons);
+    });
 });
 app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people</p>
@@ -62,26 +65,31 @@ app.delete('/api/persons/:personId', (req, res) => {
     return res.status(204).end();
 });
 app.post('/api/persons', (req, res) => {
-    let body = req.body;
+    // let body = req.body;
 
-    if (!body.name || !body.number) {
-        return res.status(400).json({
-            error: "Missing name or number"
-        });
-    } else if (persons.findIndex( person => person.name === body.name ) !== -1) {
-        return res.status(400).json({
-            error: "Name must be unique"
-        });
-    }
+    // if (!body.name || !body.number) {
+    //     return res.status(400).json({
+    //         error: "Missing name or number"
+    //     });
+    // } else if (persons.findIndex( person => person.name === body.name ) !== -1) {
+    //     return res.status(400).json({
+    //         error: "Name must be unique"
+    //     });
+    // }
 
-    let person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number
-    };
+    // let person = {
+    //     id: generateId(),
+    //     name: body.name,
+    //     number: body.number
+    // };
 
-    persons = persons.concat(person);
-    return res.json(person);
+    // persons = persons.concat(person);
+    // return res.json(person);
+    let newPerson = new Person(req.body);
+
+    newPerson.save().then(result => {
+        return res.json(result);
+    });
 });
 
 const PORT = process.env.PORT || 3001;
