74c03561f3472e3cf64b7096fd00292216eaf382
SonTrungTo <son.trung.to@gmail.com>
2020-12-03 23:47:51 +0200

    3.15-3.18 solved!

--- index.js
+++ index.js
@@ -4,33 +4,6 @@ const cors = require('cors');
 const app = express();
 const Person = require('./models/person');
 
-// let persons = [
-//     {
-//         id: 1,
-//         name: "Arto Hellas",
-//         number: "040-123456"
-//     },
-//     {
-//         id: 2,
-//         name: "Ada Lovelace",
-//         number: "39-44-5323523"
-//     },
-//     {
-//         id: 3,
-//         name: "Dan Abramov",
-//         number: "12-43-234345"
-//     },
-//     {
-//         id: 4,
-//         name: "Mary Poppendick",
-//         number: "39-23-6423122"
-//     }
-// ];
-
-const generateId = () => {
-    return Math.round(Math.random() * new Date().valueOf());
-};
-
 app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
@@ -45,52 +18,63 @@ app.get('/api/persons', (req, res) => {
     });
 });
 app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>`);
+    Person.find({})
+    .then(result => {
+        res.send(`<p>Phonebook has info for ${result.length} people</p>
+        <p>${new Date()}</p>`);
+    });
 });
-app.get('/api/persons/:personId', (req, res) => {
-    let id = Number(req.params.personId);
-    let person = persons.find(person => person.id === id);
-
-    if (person) {
-        return res.json(person);
-    } else {
-        return res.status(404).end(`Error 404: Resource does not exist`);
-    }
+app.get('/api/persons/:personId', (req, res, next) => {
+    Person.findById(req.params.personId)
+    .then(result => {
+        return res.json(result);
+    })
+    .catch(error => next(error));
 });
-app.delete('/api/persons/:personId', (req, res) => {
-    let id = Number(req.params.personId);
-    persons = persons.filter(person => person.id !== id);
-
-    return res.status(204).end();
+app.delete('/api/persons/:personId', (req, res, next) => {
+    Person.findByIdAndDelete(req.params.personId)
+    .then(result => {
+        return res.status(204).end();
+    })
+    .catch(error => next(error));
 });
 app.post('/api/persons', (req, res) => {
-    // let body = req.body;
-
-    // if (!body.name || !body.number) {
-    //     return res.status(400).json({
-    //         error: "Missing name or number"
-    //     });
-    // } else if (persons.findIndex( person => person.name === body.name ) !== -1) {
-    //     return res.status(400).json({
-    //         error: "Name must be unique"
-    //     });
-    // }
-
-    // let person = {
-    //     id: generateId(),
-    //     name: body.name,
-    //     number: body.number
-    // };
-
-    // persons = persons.concat(person);
-    // return res.json(person);
     let newPerson = new Person(req.body);
-
     newPerson.save().then(result => {
         return res.json(result);
     });
 });
+app.put('/api/persons/:personId', (req, res, next) => {
+    Person.findByIdAndUpdate(req.params.personId, {
+        name: req.body.name,
+        number: req.body.number
+    }, {new: true})
+    .then(result => {
+        return res.json(result);
+    })
+    .catch(error => next(error));
+});
+
+const unknownEndpoint = (req, res) => {
+    return res.status(404).json({
+        error: 'Unknown endpoint'
+    });
+};
+
+const errorHandler = (err, req, res, next) => {
+    console.error(err.name, err.message);
+
+    if (err.name === "CastError") {
+        return res.status(400).send({
+            error: 'Malformed personId endpoint'
+        });
+    }
+
+    next(err);
+};
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
